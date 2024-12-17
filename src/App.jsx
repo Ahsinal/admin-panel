61164e656ef0af86811ed2bdbd963a59a585848a
Ahsinal <lanishakoirala23@gmail.com>
2024-12-17 19:07:05 +0545

    added user/id and fallback and not found routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import SideNav from "./components/SideNav";
 import Analytics from "./pages/Analytics";
 import Sales from "./pages/Sales";
 import UserList from "./pages/UserList";
+import User from "./pages/User";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
@@ -16,23 +18,46 @@ function App() {
     setIsNavbarOpen(state);
   };
   const [navbarHeight, setNavbarHeight] = useState(0);
-  const[isSidenavOpen,SetIsSidenavOpen]=useState(0);
+  const [isSidenavOpen, SetIsSidenavOpen] = useState(0);
   const handleSideNav = () => {
     SetIsSidenavOpen((prev) => !prev);
   };
   return (
     <Router>
-      <Topbar onNavbarToggle={handleNavbarToggle} setNavbarHeight={setNavbarHeight}/>
+      <Topbar
+        onNavbarToggle={handleNavbarToggle}
+        setNavbarHeight={setNavbarHeight}
+      />
       <div className=" flex  ">
-      <SideNav isSidenavOpen={isSidenavOpen} />
-      <div className=" w-full">
-      <Routes>
-        <Route path="/" element={<Homepage isNavbarOpen={isNavbarOpen}/>} />
-        <Route path="/analytics" element={<Analytics isNavbarOpen={isNavbarOpen}/>} />
-        <Route path="/sales" element={<Sales isNavbarOpen={isNavbarOpen}/>} />
-        <Route path="/users" element={<UserList isNavbarOpen={isNavbarOpen}/>} />
-      </Routes>
-      </div>
+        <SideNav isSidenavOpen={isSidenavOpen} />
+        <div className=" w-full">
+          <Routes>
+            <Route
+              path="/"
+              element={<Homepage isNavbarOpen={isNavbarOpen} />}
+            />
+            <Route
+              path="/analytics"
+              element={<Analytics isNavbarOpen={isNavbarOpen} />}
+            />
+            <Route
+              path="/sales"
+              element={<Sales isNavbarOpen={isNavbarOpen} />}
+            />
+            <Route
+              path="/users"
+              element={<UserList isNavbarOpen={isNavbarOpen} />}
+            />
+            <Route
+              path="/user/:id"
+              element={<User isNavbarOpen={isNavbarOpen} />}
+            />
+            {/* Fallback route for unmatched routes */}
+            <Route path="/notfound" element={<NotFound />} />
+            {/* fallback route for undefined paths */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
