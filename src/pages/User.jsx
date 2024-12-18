4f9de71e6071e28d02739a9a1a82740f46d022a6
Ahsinal <lanishakoirala23@gmail.com>
2024-12-18 13:52:57 +0545

    added individual user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { CalendarToday, PermIdentity, PhoneAndroid } from "@mui/icons-material";
 
 const User = () => {
   return (
-    <div>User</div>
-  )
-}
+    <div className="p-12">
+      <div className=" flex items-center justify-between">
+        <h1 className="text-lg text-gray-800 font-semibold">Edit User</h1>
+        <button className="bg-teal-600 px-5 py-2 rounded-md text-white outline-none  hover:bg-teal-700">
+          {" "}
+          Create
+        </button>
+      </div>
+      <div className="flex  justify-between  space-x-4 my-4">
+        <div className="shadow-lg bg-slate-50 p-4 rounded-md w-2/5 sm:w-1/2 md:w-1/4">
+          <div className="flex items-center space-x-4">
+            <img
+              src=""
+              alt="avatar"
+              className="w-14 h-14 items-center rounded-full"
+            />
+            <div className="">
+              <p className="font-bold text-gray-700"> Anna Zukerberg</p>
+              <p className="text-sm font-semibold text-gray-600">
+                Software Engineer
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <p className="font-bold text-cGray-700">Account Details</p>
+            <div className="flex space-x-2 p  items-center  text-gray-600">
+              <PermIdentity />
+              <span>ananabeck99</span>
+            </div>
+            <div className="flex space-x-2   items-center  text-gray-600">
+              <CalendarToday />
+              <span>2025-04-5</span>
+            </div>
+            <div className="flex space-x-2  items-center  text-gray-600">
+              <PhoneAndroid />
+              <span>9999999999</span>
+            </div>
+            <div className="flex space-x-2  items-center  text-gray-600">
+              <PermIdentity />
+              <span>ananabeck99</span>
+            </div>
+          </div>
+        </div>
 
-export default User
+        <div className="shadow-lg bg-slate-50 p-4 rounded-md w-3/5 sm:w-1/2 md:w-3/4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,76 +1,85 @@
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { DeleteOutline, Edit } from "@mui/icons-material";
 import { userRows } from "../assets/data/userData";
 import { Link } from "react-router-dom";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "user",
-    headerName: "User",
-    width: 250,
-    renderCell: (params) => {
-      return (
-        <div className="flex items-center space-x-4">
-          <img
-            src={params.row.avatar}
-            alt="avatar"
-            className="w-8 h-8 items-center rounded-full"
-          />
-          <p>{params.row.user}</p>
-        </div>
-      );
-    },
-  },
-  {
-    field: "email",
-    headerName: "Email",
-    type: "email",
-    width: 160,
-  },
-  {
-    field: "status",
-    headerName: "Status",
-    width: 110,
-    renderCell: (params) => {
-      const status = params.value ? params.value.toLowerCase() : "null "; // default to 'unknown' if undefined
-      const statusClass =
-        status === "active"
-          ? " text-green-500  "
-          : status === "blocked"
-          ? "text-red-500 "
-          : " text-gray-700";
-      return <button className={`  ${statusClass}`}>{status}</button>;
-    },
-  },
-  {
-    field: "transaction",
-    headerName: "Transaction",
-    width: 110,
-  },
-  {
-    field: "action",
-    headerName: "Action",
-    type: "number",
-    width: 150,
-    renderCell: (params) => {
-      return (
-        <div className="flex items-center justify-center space-x-2">
-          <Link to={'/user/'+params.row.id}>
-            <button>
-              <Edit className="text-green-600 cursor-pointer" />
-            </button>
-          </Link>
-          <DeleteOutline className="text-red-600 cursor-pointer" />
-        </div>
-      );
-    },
-  },
-];
-
 const UserList = () => {
+  const [data, setData] = useState(userRows);
+  const handleDeleteUser = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <div className="flex items-center space-x-4">
+            <img
+              src={params.row.avatar}
+              alt="avatar"
+              className="w-8 h-8 items-center rounded-full"
+            />
+            <p>{params.row.user}</p>
+          </div>
+        );
+      },
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      type: "email",
+      width: 160,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 110,
+      renderCell: (params) => {
+        const status = params.value ? params.value.toLowerCase() : "null "; // default to 'unknown' if undefined
+        const statusClass =
+          status === "active"
+            ? " text-green-500  "
+            : status === "blocked"
+            ? "text-red-500 "
+            : " text-gray-700";
+        return <button className={`  ${statusClass}`}>{status}</button>;
+      },
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction",
+      width: 110,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      type: "number",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div className="flex items-center justify-center space-x-2">
+            <Link to={"/user/" + params.row.id}>
+              <Edit className="text-green-600 cursor-pointer outline-none" />
+            </Link>
+            <DeleteOutline
+              onClick={() => handleDeleteUser(params.row.id)}
+              className="text-red-600 cursor-pointer"
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+  const handleCellClick = (params, event) => {
+    // Prevent the default row selection behavior
+    event.stopPropagation();
+  };
   return (
     <section className=" p-6">
       <h1 className="text-lg px-4 py-2  my-3 text-white  w-auto bg-green-600 inline-block">
@@ -78,8 +87,20 @@ const UserList = () => {
       </h1>
       <Box className="container mx-auto">
         <DataGrid
-          rows={userRows}
+          rows={data}
           columns={columns}
+          onCellClick={handleCellClick}
+          sx={{
+            ".MuiDataGrid-cell:focus": {
+              outline: "none", // Removes focus outline
+            },
+            ".MuiDataGrid-row.Mui-selected": {
+              backgroundColor: "transparent", // Removes row selection background
+            },
+            ".MuiDataGrid-row:hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.04)", // Keeps row hover effect
+            },
+          }}
           initialState={{
             pagination: {
               paginationModel: {
